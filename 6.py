4d711261b2ced67ebc717b9697a6420c33fd8bed
thejuspk07 <thejusit6@gmail.com>
2025-10-27 22:43:32 +0530

    new file

--- 6.py
+++ 6.py
@@ -2,4 +2,10 @@
 a = int(input("enter the first number:"))
 b = int(input("enter the second number:"))
 c = int(input("enter the third number:"))
-if a
+if a >= b and  a >= c:
+    print(+a,"is larger")
+elif  b>=a and b >= c:
+    print(+b,"is larger")
+else:
+    print(+c,"is greater")
+
