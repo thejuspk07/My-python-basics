09a20d904c5e2cb001888e3ac6138e2d3c595a7b
thejuspk07 <thejusit6@gmail.com>
2025-11-29 12:46:59 +0530

    updated

---
+++ My-python-basics/python-basic/3-Python Conditional Statements (if, elif, else)/if-elif-else.py
@@ -0,0 +1,7 @@
+marks = float(input("Enter the Marks:"))
+if marks>=90:
+    print("A")
+elif marks>=70:
+    print("B")
+else:
+    print("C")
--- My-python-basics/python-basic/3-Python Conditional Statements (if, elif, else)/if-else statement.py
+++ My-python-basics/python-basic/3-Python Conditional Statements (if, elif, else)/if-else statement.py
@@ -1,4 +1,4 @@
-age = int(input("enter the age"))
+age = int(input("enter the age:"))
 if age>=18:
     print("Adult")
 else:
