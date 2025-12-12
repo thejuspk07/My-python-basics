c2647c3a21c6cfe511958be0a192ce6b64f8e409
thejuspk07 <thejusit6@gmail.com>
2025-12-12 22:39:13 +0530

    added basic.py

---
+++ basics.py
@@ -0,0 +1,55 @@
+#variables should start with
+# Alphabets,numbers and
+# no other special characters are allowed
+# Case sensitive
+# Numbers are not allowed in beginning
+num = 10
+print(num)
+
+# Numerical
+    #int
+    #float
+    #complex
+
+#type
+print(type(num))
+
+#float
+a = 10.0
+print(type(a))
+
+#complex
+a  = 10+10j
+print(type(a))
+
+
+# Sequence
+    # string
+    # list
+    # tuple
+    # Range
+
+# set type
+    #sets
+
+# mapping type
+    #dictionary
+
+
+# None type
+    #none
+
+
+
+#length
+a = "python lang"
+print(len(a))
+#ordered
+#duplicated are allowed
+#immutable
+
+name ="Thejus"
+age =20
+place ="kannur"
+qualification = "B tech"
+print(f"Iam {name}.Iam coming from {place}.Iam {age} years old.\nMy qualification is {qualification}")
