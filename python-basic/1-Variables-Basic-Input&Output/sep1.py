9581c67142b0f0714a0bab4bfc47ec036fbda4ec
thejuspk07 <thejusit6@gmail.com>
2025-12-13 20:16:15 +0530

    added new programs

--- python-basic/1-Variables-Basic-Input&Output/sep1.py
+++
@@ -1,8 +0,0 @@
-#rint the values "apple", "banana", "cherry" separated by " | ".
-print("apple ",'banana '," cherry ",sep="|",end=" ..")
-print()
-
-#. Print the values 2025, 09, 22 separated by "-".
-print("2025","09","22",sep="-")
-
-
---
+++ python-basic/1-Variables-Basic-Input&Output/seperator.py
@@ -0,0 +1,14 @@
+#rint the values "apple", "banana", "cherry" separated by " | ".
+print("apple ",'banana '," cherry ",sep="|")
+print()
+
+#. Print the values 2025, 09, 22 separated by "-".
+print("2025","09","22",sep="-")
+
+
+#. Print "Red", "Green", "Blue" separated by newline (\n).
+print(" Red\n","Green\n","Blue\n")
+
+
+#Print numbers 1, 2, 3, 4, 5 separated by " --> "
+print("1","2","3","4","5",sep="-->")
