7bc71b2ae6cb88b7c3c25050c191d39712c453d9
thejuspk07 <thejusit6@gmail.com>
2025-12-13 20:24:44 +0530

    added new programs

---
+++ python-basic/1-Variables-Basic-Input&Output/End statements.py
@@ -0,0 +1,10 @@
+#Print "Hello" and "World" in the same line using two print() statements
+print("Hello",end ="")
+print("World")
+
+# Print "Python", "is", "fun" each with separate print() but all in one line separated by spaces.
+print("python ",end = "")
+print(" is",end="")
+print(" fun")
+
+
