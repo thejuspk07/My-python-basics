db6e70c3daa6ffa578bf2cb829ea2be3a444c46d
thejuspk07 <thejusit6@gmail.com>
2025-12-15 21:43:35 +0530

    added new programs

---
+++ python-basic/1-Variables-Basic-Input&Output/Three Words.py
@@ -0,0 +1,5 @@
+#Ask the user to enter three words. Print them separated by " - " using sep.
+a = input("Enter first word:")
+b = input("Enter second word:")
+c = input("Enter third word:")
+print(a,b,c,sep="-")
