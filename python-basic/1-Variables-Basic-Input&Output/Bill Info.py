e5d4962be65042454e637cc76157ba2cdf368294
thejuspk07 <thejusit6@gmail.com>
2025-12-20 10:12:30 +0530

    added some code

---
+++ python-basic/1-Variables-Basic-Input&Output/Bill Info.py
@@ -0,0 +1,4 @@
+#Ask for customer name and bill amount. Print using an f-string:
+name = str(input("Enter the name"))
+amount = int(input("Enter the amount"))
+print(f"Dear {name},your bill is {amount} rupees")
