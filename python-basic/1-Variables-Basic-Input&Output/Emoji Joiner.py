49d5ec9145af20cb24fc66e31bd56332dd66ee86
thejuspk07 <thejusit6@gmail.com>
2025-12-20 10:16:37 +0530

    added some code

---
+++ python-basic/1-Variables-Basic-Input&Output/Emoji Joiner.py
@@ -0,0 +1,5 @@
+#Ask for three favorite emojis. Print them in one line separated by spaces.
+emoji1=input("enter first emoji:")
+emoji2=input("enter second emoji:")
+emoji3=input("enter third emoji:")
+print(emoji1,emoji2,emoji3,sep=" ")
