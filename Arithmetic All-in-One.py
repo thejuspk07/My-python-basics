b3bc883ecbb47812e1bd267e61596b768c2c09f9
thejuspk07 <thejusit6@gmail.com>
2025-12-20 20:51:16 +0530

    added some code

---
+++ Arithmetic All-in-One.py
@@ -0,0 +1,8 @@
+#Input two numbers. Print their sum, difference, product, and quotient in one line separated by " | ".
+num1=int(input("Enter the number:"))
+num2=int(input("Enter the number:"))
+sum=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+print(sum,sub,mul,div,sep= "|")
---
+++ Average of Three Numbers.py
@@ -0,0 +1,7 @@
+#Ask for three numbers. Print their average in this format:
+num1=int(input("Enter first number:"))
+num2=int(input("Enter second number:"))
+num3=int(input("Enter third number:"))
+total=num1+num2+num3
+avg=total/3
+print(f"The average of {num1},{num2},{num3} is {avg}")
