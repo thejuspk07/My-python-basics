eeb293df45604245f7844859ac89f4c86a19e0fc
thejuspk07 <thejusit6@gmail.com>
2025-12-20 10:04:37 +0530

    added some code

---
+++ python-basic/1-Variables-Basic-Input&Output/Poem Line Join.py
@@ -0,0 +1,5 @@
+#Ask for two short lines of text. Print them together on the same line using end.
+a=input("enter a short line of text:")
+b=input("enter a short line of text:")
+print(a,end=" ")
+print(b)
