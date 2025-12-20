74c9d4043f29b86d5694aa79804c220a27d6ac56
thejuspk07 <thejusit6@gmail.com>
2025-12-20 09:42:42 +0530

    added some code

---
+++ python-basic/1-Variables-Basic-Input&Output/Simple Introduction.py
@@ -0,0 +1,5 @@
+#Ask for name, age, and place. Print a formatted introduction:
+name = input("Enter your name:")
+age = input("Enter your age:")
+place = input("Enter your place:")
+print(f"My name is {name}. I am {age} years old . I live in {place}")
