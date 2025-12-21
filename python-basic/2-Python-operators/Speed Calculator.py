6ad81afd5dda88f2be9e2ca673ae8a8b6e88734c
thejuspk07 <thejusit6@gmail.com>
2025-12-21 20:39:00 +0530

    added some code

---
+++ python-basic/2-Python-operators/Speed Calculator.py
@@ -0,0 +1,5 @@
+#Input distance (in km) and time (in hours). Calculate speed using:
+distance = int(input("Enter the distance:"))
+time = int(input("Enter the time:"))
+speed = distance/time
+print(f"speed is {speed} Km/h")
