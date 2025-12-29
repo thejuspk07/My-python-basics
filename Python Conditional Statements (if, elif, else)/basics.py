a4d843c97d4fdc8b9df064371e86c87bfa8dac02
thejuspk07 <thejusit6@gmail.com>
2025-12-29 22:19:08 +0530

    updated

---
+++ Python Conditional Statements (if, elif, else)/basics.py
@@ -0,0 +1,13 @@
+#nested loop
+age = int(input("Enter the age:"))
+if age>=18:
+    res=input("Did you passed driving test")
+    if res=='yes'or 'YES':
+        print("You are eligible")
+    else:
+        print("You are not eligible")
+else:
+    print("Age not eligible")
+
+
+
--- basics.py
+++ basics.py
@@ -130,3 +130,16 @@ print(a+b) # ans =3434  #string concatenation
 a = "Nikil"
 b = "Madhev"
 print(a+" "+b)
+
+
+#nested loop
+age = int(input("Enter the age:"))
+if age>=18:
+    res=input("Did you passed driving test")
+    if res=='yes'or 'YES':
+        print("You are eligible")
+    else:
+        print("You are not eligible")
+else:
+    print("Age not eligible")
+
