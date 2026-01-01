def879f199d2ba10747f6c011ee931ef451bb909
thejuspk07 <thejusit6@gmail.com>
2026-01-01 20:50:13 +0530

    updated

--- Python Conditional Statements (if, elif, else)/basics.py
+++ Python Conditional Statements (if, elif, else)/basics.py
@@ -10,4 +10,40 @@ else:
     print("Age not eligible")
 
 
+#another one
+n = int(input("Enter a number"))
+if n>0:
+    if n%2==0:
+        print("positive even number")
+    else:
+        print("positive odd number")
+elif n<0:
+    if n%2==0:
+        print("Negative even number")
+    else:
+        print("Negative odd number")
+else:
+    print("Zero")
 
+#another
+user="user1"
+pwd="abc"
+user_guess=input("Enter user name:")
+password=input("Enter the password:")
+if user_guess==user:
+    if password==pwd:
+        print("login successful")
+    else:
+        print("incorrect password")
+else:
+    print("incorrect username")
+
+#another
+marks = int(input("enter the marks"))
+if marks>=40:
+    if marks>=75:
+        print("Distinction")
+    else:
+        print("pass")
+else:
+    print("fail")
