39d8a6a2d52ab54ce3d244bec0aebc26bfe3e54f
thejuspk07 <thejusit6@gmail.com>
2026-01-02 17:32:34 +0530

    added new code

---
+++ Python Conditional Statements (if, elif, else)/Pet Talk.py
@@ -0,0 +1,8 @@
+user = input("Do you have pet?(yes|no):")
+if user == "YES" or user== "yes":
+    print("Wow pets are awesome")
+elif user =="NO" or user=="no":
+    print("You should get one they are cute!")
+else:
+    print("Invalid input")
+
