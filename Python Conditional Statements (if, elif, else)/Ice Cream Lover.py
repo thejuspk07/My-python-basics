327e20d6d11ec8745c299d422346eff9cccd9bbb
thejuspk07 <thejusit6@gmail.com>
2026-01-02 17:40:22 +0530

    added new code

--- Python Conditional Statements (if, elif, else)/Ice Cream Lover.py
+++ Python Conditional Statements (if, elif, else)/Ice Cream Lover.py
@@ -1,2 +1,7 @@
 icecream = input("Which flavor of icecream you want?:")
-if icecream == ""
+if icecream == "chocolate" or icecream =="CHOCOLATE":
+    print("Same here chocolate is best")
+elif icecream == "vanilla" or icecream=="VANILLA":
+    print("Classic choice!")
+else:
+    print("Interesting! I should try that flavor")
