f87b28745cf2e0beccbb538745e11bee45dbd20a
thejuspk07 <thejusit6@gmail.com>
2026-01-03 22:15:08 +0530

    added new code

---
+++ Python Conditional Statements (if, elif, else)/Funny Password Check.py
@@ -0,0 +1,7 @@
+password =input("enter password")
+if password=="1234":
+    print("Seriously,That's too weak")
+elif password=="password":
+    print("C’mon, everyone uses that ")
+else:
+    print("good choice!")
