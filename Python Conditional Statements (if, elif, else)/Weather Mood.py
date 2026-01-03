3dbd7fb5d1f72b92b69c76279f93b1f258432853
thejuspk07 <thejusit6@gmail.com>
2026-01-03 21:46:39 +0530

    added new code

--- Python Conditional Statements (if, elif, else)/Weather Mood.py
+++ Python Conditional Statements (if, elif, else)/Weather Mood.py
@@ -1,1 +1,9 @@
-weather = input("Enter today's weather")
+weather = input("Enter today's weather(sunny,rainy,cold):")
+if weather=="sunny":
+    print("Wear sunglasses 😎")
+elif weather=="rainy":
+    print("Take an umbrella ☔")
+elif weather=="cold":
+    print("Wear a sweater")
+else:
+    print("Invalid input")
