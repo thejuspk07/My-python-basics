e0079092b42d8ae91d52380586ca0a3044378afd
thejuspk07 <thejusit6@gmail.com>
2026-01-06 21:08:58 +0530

    added new code

--- String_manpulation/basics.py
+++ String_manpulation/basics.py
@@ -2,8 +2,13 @@ s="python_programming"
 print(s[-1]) #backward
 print(s[17]) #forward
 
+
+#negitive index start at last
+#positive index start at first
+
 #slicing
 print(s[0:6]) #positive
 print(s[-18:-12]) #negative
 
 print(s[7:10])
+print(s[-3:])
