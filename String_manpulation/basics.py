42b0c653ea2af7f34780b4a3c086cae53fb3899a
thejuspk07 <thejusit6@gmail.com>
2026-01-06 21:52:36 +0530

    code

--- String_manpulation/basics.py
+++ String_manpulation/basics.py
@@ -11,6 +11,7 @@ print(s[17]) #forward
 #negative--->18
 #g--->-1,17 #ing
 
+print(len(s))
 
 #slicing
 print(s[0:6]) #positive
@@ -19,5 +20,10 @@ print(s[-18:-12]) #negative
 print(s[7:10])
 print(s[-3:])
 
+print(s[16:18])
 
-print(s[0:5:2])#pto
+print(s[0:5:2]) # positive #pto
+print(s[-1:-4:-1])# gni
+
+print(s[::-1])#right to left
+
