72965946507bfd98320057f747a57ff01a7bed98
thejuspk07 <thejusit6@gmail.com>
2026-01-07 12:55:19 +0530

    code

--- String_manpulation/basics.py
+++ String_manpulation/basics.py
@@ -1,4 +1,5 @@
-s="python_programming"
+#indexing..
+s="python programming"
 print(s[-1]) #backward
 print(s[17]) #forward
 
@@ -22,8 +23,33 @@ print(s[-3:])
 
 print(s[16:18])
 
-print(s[0:5:2]) # positive #pto
+print(s[0:5:2]) # positive #pto #step value =2
 print(s[-1:-4:-1])# gni
 
 print(s[::-1])#right to left
+print(s[::-2])
 
+#method-->a particular datatype function
+#-->lower
+#-->upper
+#-->capitalize
+#-->split
+#-->strip
+
+
+print(s.upper()) #uppercase
+print(s.lower())#lowercase
+print(s.capitalize())#first character (if alphabet) then first character become captial
+print(s.title())
+print(s.split()) #if space are available it split and makes list ['python', 'programming']
+print(len(s.split()))
+print((s.split()[1]))
+
+a="python,programming"
+print(a.split())#['python,programming']
+print(a.split(","))#['python', 'programming']
+print(a.split("n"))#['pytho', ',programmi', 'g']
+
+
+b="user@gmail.com"
+print(b.split("@")[0])#dont need @gmail part
