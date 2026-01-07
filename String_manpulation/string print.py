b82553807376cedcda28f3b133a01b0749ec15ec
thejuspk07 <thejusit6@gmail.com>
2026-01-07 20:10:56 +0530

    code

---
+++ String_manpulation/string print.py
@@ -0,0 +1,12 @@
+#Take a string input (like "PythonProgramming") and print
+#First character
+#Last character
+#Middle character
+
+a="PythonProgramming"
+length=print(len(a))
+First_character=print(a[0])
+Last_character=print(a[16])
+Middle=len(a)//2
+Middle_character=print(a[Middle])
+
