5de53996d15a2ce759e16c63083349249468e1e7
thejuspk07 <thejusit6@gmail.com>
2026-01-08 20:36:41 +0530

    code

---
+++ String_manpulation/checking first and last string same.py
@@ -0,0 +1,7 @@
+#Input a string and check if the first and last characters are the same.
+s=input("Enter a sentence:")
+len(s)
+if s[0]==s[-1]:
+    print("first and characters are same")
+else:
+    print("not same")
