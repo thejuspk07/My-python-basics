160b60154c2b4ea435dc4872dbcb8a0ffd841c47
thejuspk07 <thejusit6@gmail.com>
2026-01-09 22:35:04 +0530

    code

---
+++ String_manpulation/Remove extra space using strip.py
@@ -0,0 +1,3 @@
+# Input a string with extra spaces at the beginning and end. Remove them using strip() and print the cleaned string
+s=input("Enter a sentence  with spaces at beginning and end:")
+print(s.strip())
