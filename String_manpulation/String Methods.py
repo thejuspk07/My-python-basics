a1588695f9de668d7179f8598dce01e5de384b29
thejuspk07 <thejusit6@gmail.com>
2026-01-09 22:35:12 +0530

    code

---
+++ String_manpulation/String Methods.py
@@ -0,0 +1,11 @@
+#Take a name input and display it in:
+#Uppercase
+#Lowercase
+#Title case
+#Capitalized
+
+a=input("Enter a name:")
+print(a.upper())
+print(a.lower())
+print(a.title())
+print(a.capitalize())
