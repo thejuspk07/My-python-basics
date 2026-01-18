daa6f402453ae7ab8f6972657092f0b7b21755ce
thejuspk07 <thejusit6@gmail.com>
2026-01-18 21:08:34 +0530

    code

--- Loops/for_loop/practice.py
+++ Loops/for_loop/practice.py
@@ -5,4 +5,10 @@ for i in range(1,n+1):
     fact*=i
 print(fact)
 
-# write a program to
+# write a program to calculate the sum of digits of a given number
+#eg input:7123 ---> Expected result:13
+#iterable things --> strings,list,range
+#integer float not iterable
+
+s = int(input("enter the number:"))
+for i in s:
