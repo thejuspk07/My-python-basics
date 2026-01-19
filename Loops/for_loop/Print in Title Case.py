e6d8a4aa51cbe8bcfaf90d16d83f38d67c450425
thejuspk07 <thejusit6@gmail.com>
2026-01-19 21:57:08 +0530

    code

---
+++ Loops/for_loop/Print in Title Case.py
@@ -0,0 +1,9 @@
+#Given a list of messy names:
+
+#names = ["arya nair", "john DOE", "meera mathew"]
+
+#Print each name neatly in title case.
+
+names = ["arya nair", "john DOE", "meera mathew"]
+for i in names:
+    print(i.title())
