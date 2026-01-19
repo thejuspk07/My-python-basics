905e27dd59654a4fae48cdd65373f82a20852f0c
thejuspk07 <thejusit6@gmail.com>
2026-01-19 21:57:01 +0530

    code

---
+++ Loops/for_loop/loop in list.py
@@ -0,0 +1,7 @@
+#Given a list:
+#fruits = ["apple", "banana", "cherry", "mango"]
+#Print each fruit using a for loop.
+fruits = ["apple", "banana", "cherry", "mango"]
+for i in fruits:
+    print(i)
+
