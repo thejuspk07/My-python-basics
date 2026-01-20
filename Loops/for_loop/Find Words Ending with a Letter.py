8aacfd94222bed56f6ba2f43ee36a3cb01021a94
thejuspk07 <thejusit6@gmail.com>
2026-01-20 22:00:35 +0530

    code

---
+++ Loops/for_loop/Find Words Ending with a Letter.py
@@ -0,0 +1,6 @@
+#Input a letter from the user and print all fruits that end with that letter using endswith().
+l=input("enter the letter:")
+words = ["apple", "pineapple", "kiwi", "strawberry", "fig"]
+for i in words:
+    if i.endswith(l):
+        print(i)
