9e06228ff7a607b373181b78ed0e692cbc3465a2
thejuspk07 <thejusit6@gmail.com>
2026-01-20 22:20:02 +0530

    code

---
+++ Loops/for_loop/Replace Character.py
@@ -0,0 +1,7 @@
+#Given:
+#words = ["python", "practice", "programming"]
+#Replace every "p" with "P" and print the new list.
+words = ["python", "practice", "programming"]
+for i in words:
+    s=i.replace("p","P")
+    print(s)
