a3df1b20a3ad0fdbfd2eda35bee66e97353a5f93
thejuspk07 <thejusit6@gmail.com>
2026-01-20 22:11:06 +0530

    code

---
+++ Loops/for_loop/Filter Words Starting with a Letter.py
@@ -0,0 +1,17 @@
+#From a list of city names, print only the cities that start with "B" using startswith().
+cities = [
+    "Bengaluru",
+    "Delhi",
+    "Bhopal",
+    "Mumbai",
+    "Berlin",
+    "Chennai",
+    "Boston",
+    "Kochi",
+    "Barcelona",
+    "Pune"
+]
+
+for i in cities:
+    if i.startswith("B"):
+        print(i)
