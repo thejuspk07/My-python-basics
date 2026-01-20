11ecddf689ac833168e8b414b188744de6a78ba3
thejuspk07 <thejusit6@gmail.com>
2026-01-20 21:51:50 +0530

    code

---
+++ Loops/for_loop/Count Long Words.py
@@ -0,0 +1,10 @@
+#Given:
+#words = ["apple", "pineapple", "kiwi", "strawberry", "fig"]
+#Count and print how many words have more than 5 letters.
+words = ["apple", "pineapple", "kiwi", "strawberry", "fig"]
+count=0
+for i in words:
+    if len(i)>5:
+        print(i)
+        count+=1
+print(count)
