dc9df933e8d040c5ab9a25d4a898574963a9f807
thejuspk07 <thejusit6@gmail.com>
2026-01-21 21:28:52 +0530

    code

---
+++ Loops/for_loop/Shorten Each Word.py
@@ -0,0 +1,6 @@
+#Given:
+#animals = ["elephant", "lion", "tiger", "giraffe"]
+#Print only the first 3 letters of each animal.
+animals = ["elephant", "lion", "tiger", "giraffe"]
+for i in animals:
+    print(i[:3])
