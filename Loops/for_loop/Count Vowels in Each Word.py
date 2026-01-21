3e63455a4bc109d39be60e6380dcaf76cce42b23
thejuspk07 <thejusit6@gmail.com>
2026-01-21 21:08:38 +0530

    code

---
+++ Loops/for_loop/Count Vowels in Each Word.py
@@ -0,0 +1,10 @@
+#For each word in:
+#words = ["apple", "orange", "grape"]
+#Count the vowels using a for loop and print:
+#> apple → 2
+#orange → 3
+#grape → 2
+words = ["apple", "orange", "grape"]
+vowels="aeiou"
+for i in words:
+    if (i)==
