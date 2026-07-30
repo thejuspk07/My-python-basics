ed111ce0784b7c9e273168953aa58ce036a485e2
thejuspk07 <thejusit6@gmail.com>
2026-07-30 22:56:59 +0530

    added some stuffs

--- t.py
+++ t.py
@@ -1,9 +1,9 @@
-l=[]
-num=7
-for i in range(2,num):
-    for i in range(2,num//2):
-        if num%i==0:
-            l.append(num)
-            break
-print(l)
+n=7
+for i in range(2,n):
+    if n%i==0:
+        print("not prime number")
+        break
+else:
+        print(" prime number ")
+
 
