11ce28a09ba9dac8007374a41518a64b12e441da
thejuspk07 <thejusit6@gmail.com>
2026-08-20 10:46:25 +0530

    added some stuffs

--- Loops/test.py
+++ Loops/test.py
@@ -1,5 +1,7 @@
-s="hello"
-l=""
-for i in s:
-    l=i+l
-print(l)
+companies=["Indian oil","state bank of india","tata motors"]
+for i in companies:
+    s=""
+    for j in i.split():
+        if len(j)>2:
+            s+=j[0].upper()
+    print(s)
