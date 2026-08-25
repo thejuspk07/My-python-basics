383bc1891937c660d67b0242077c8c9d918c1cca
thejuspk07 <thejusit6@gmail.com>
2026-08-25 21:13:00 +0530

    added some stuffs

---
+++ Loops/for_loop/Words Containing Letter.py
@@ -0,0 +1,5 @@
+# Print all words from a given list that contain the letter "a".
+companies = ["Indian Oil", "State Bank of India", "Tata Motors"]
+for i in companies:
+    if "a" in i.lower():
+        print(i)
--- Loops/test.py
+++ Loops/test.py
@@ -1,7 +1,6 @@
-#write a program to calculate the sum of digits
-n=3123
-s=0
-for i in str(n):
-    s+=int(i)
-print(s)
-
+#factorial using loop
+n= int(input("enter the number:"))
+fact=1
+for i in range(1,n+1):
+    fact=fact*i
+print(fact)
