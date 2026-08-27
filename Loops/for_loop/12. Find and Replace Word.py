9072ee690584dfc88f8768725d9bf48406b3039c
thejuspk07 <thejusit6@gmail.com>
2026-08-27 20:38:14 +0530

    added some stuffs

---
+++ Loops/for_loop/12. Find and Replace Word.py
@@ -0,0 +1,8 @@
+# colors = ["red", "blue", "green", "blue"]
+#
+# Replace all "blue" with "skyblue" and print the updated list.
+colors = ["red", "blue", "green", "blue"]
+for i in range(len(colors)):
+    if colors[i]=='blue':
+        colors[i]='skyblue'
+print(colors)
