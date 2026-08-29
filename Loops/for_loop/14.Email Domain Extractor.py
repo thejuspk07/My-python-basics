8de60b423e9c8331c35a153800e236f3fa2d3f8a
thejuspk07 <thejusit6@gmail.com>
2026-08-29 22:31:55 +0530

    added some stuffs

---
+++ Loops/for_loop/14.Email Domain Extractor.py
@@ -0,0 +1,9 @@
+# Given a list of emails:
+#
+# emails = ["arya@gmail.com", "meera@yahoo.com", "john@outlook.com"]
+#
+# Print only the domain part (after @) for each email
+emails = ["arya@gmail.com", "meera@yahoo.com", "john@outlook.com"]
+for i in emails:
+    domain=i.split('@')
+    print(domain[1])
