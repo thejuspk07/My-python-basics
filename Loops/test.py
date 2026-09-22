8fe67f961890362f6e437088e1223352104bafe5
thejuspk07 <thejusit6@gmail.com>
2026-09-22 12:25:21 +0530

    added some stuffs

--- Loops/test.py
+++ Loops/test.py
@@ -1,4 +1,8 @@
 # write a program to calculate the sum of digits in a given number
 # eg input: 7123 ---? expected result :13
 #             811--> expected result :10
-aaa
+n="7123"
+res=0
+for i in n:
+    res+=int(i)
+print(res)
