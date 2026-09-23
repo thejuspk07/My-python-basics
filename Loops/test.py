63ce0187aef6df1d3711c65cde8b220b925db673
thejuspk07 <thejusit6@gmail.com>
2026-09-23 21:03:10 +0530

    added

--- Loops/test.py
+++ Loops/test.py
@@ -1,11 +1,2 @@
-# write a program to calculate the sum of digits in a given number
-# eg input: 7123 ---? expected result :13
-#             811--> expected result :10
-n="7123"
-res=0
-for i in n:
-    res+=int(i)
-print(res)
-
-
-a
+#armstrong number
+ll
